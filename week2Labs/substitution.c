0205a30f5a75223263beaa3654bffb3d89351d10
Zachariah Kuczynski <81714702+Krazarak@users.noreply.github.com>
2021-12-30 14:43:52 -0600

    Add files via upload

--- week2Labs/substitution.c
+++ week2Labs/substitution.c
@@ -6,21 +6,21 @@ int main(int argc, string argv[])
 {
     const int ALPHABET_LEN = 26;
     const string ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-// If no or more than 1 command-line argument is passed in, return error
+    // Check for error - if none or more than 1 command-line argument is passed in
     if (argc != 2)
     {
         printf("Usage: ./substitution key\n");
         return 1;
     }
-// ** If key is invalid (not 26 chars, not alphabetical, & not containing each letter exactly once), return errors
+    // Check validity (26 char, not alphabetical, & not containing a letter once)
     int argLength = strlen(argv[1]);
-// Check if key is 26 characters
+    // Check if key is 26 characters
     if (argLength != ALPHABET_LEN)
     {
         printf("Key must contain 26 characters.\n");
         return 1;
     }
-for (int x = 0, len = strlen(argv[1]); x < len; x++)
+    for (int x = 0, len = strlen(argv[1]); x < len; x++)
     {
         // Check if character is alphabetical
         if (!isalpha(argv[1][x]))
@@ -28,7 +28,7 @@ for (int x = 0, len = strlen(argv[1]); x < len; x++)
             printf("Key must be alphabetical.\n");
             return 1;
         }
-// Check if each letter is only used once
+        // Check if each letter is only used once
         int letters[ALPHABET_LEN];
         for (int j = 0; j < ALPHABET_LEN; j++)
         {
@@ -42,9 +42,9 @@ for (int x = 0, len = strlen(argv[1]); x < len; x++)
         // Stores character for next loop to check for match
         letters[x] = argv[1][x];
     }
-string plaintext = get_string("plaintext: ");
+    string plaintext = get_string("plaintext: ");
     char ciphertext[strlen(plaintext) + 1];
-for (int i = 0, len = strlen(plaintext); i < len; i++)
+    for (int i = 0, len = strlen(plaintext); i < len; i++)
     {
         // if uppercase...
         if (isupper(plaintext[i]))
@@ -80,6 +80,6 @@ for (int i = 0, len = strlen(plaintext); i < len; i++)
     }
     // turn char into string
     ciphertext[strlen(plaintext)] = '\0';
-printf("ciphertext: %s\n", ciphertext);
-return 0;
+    printf("ciphertext: %s\n", ciphertext);
+    return 0;
 }
